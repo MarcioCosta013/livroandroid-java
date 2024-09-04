b200fb5acfade835cfca22eae33af21430ff362f
Marcio Costa <marcio-costa38@hotmail.com>
2024-09-04 19:00:06 -0300

    adicionando o pacote cap7

---
+++ cap_7/app/src/main/java/br/livro/android/cap07/recursos/ExemploTextoCores.java
@@ -0,0 +1,15 @@
+package br.livro.android.cap07.recursos;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ExemploTextoCores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.texto_cores);
+
+    }
+}
